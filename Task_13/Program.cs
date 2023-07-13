71a38fbeb7ea0ba6b19457901dea88644cb926e3
Anna <vitani2007@mail.ru>
2023-07-13 13:06:37 +0300

    all folders added

---
+++ Task_13/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number / 100 == 0)
+{
+    Console.WriteLine($"{number} -> третьей цифры нет");
+}
+
+if (number / 100 >= 1 & number / 100 < 10)
+{
+    int firstDigit = number / 100;
+    int thirdDigit = (number - firstDigit * 100) % 10;
+    Console.WriteLine($"{number} -> {thirdDigit}");
+}
+
+if (number / 100 >= 10)
+{
+    int x = number / 10;
+    while (x > 999)
+    {
+        x = x / 10;
+    }
+    int firstDigit = x / 100;
+    int thirdDigit = (x - firstDigit * 100) % 10;
+    Console.WriteLine($"{number} -> {thirdDigit}");
+}
---
+++ Task_15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number ==6 | number ==7)
+{
+    Console.WriteLine($"{number} -> да");
+}
+
+else 
+{
+    Console.WriteLine($"{number} -> нет");
+}
