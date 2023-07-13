2f1b44b870acf2d9534c3117fd5b1fdfead43bca
Anna <vitani2007@mail.ru>
2023-07-13 13:02:38 +0300

    task 13 solved

---
+++ Task-10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int firstDigit = number / 100;
+int secondDigit = (number - firstDigit * 100) / 10;
+
+Console.WriteLine($"{number} -> {secondDigit}");
